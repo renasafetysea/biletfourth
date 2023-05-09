ac908342595b0d61932aad6f01a0892ac574faf9
Anna Molodtsova <renasafetysea@mail.ru>
2023-05-09 17:59:28 +0300

    решение

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "я могу делить строку на части";
+        String[] words = str.split(" ");
+        for(String word: words){
+            System.out.println(word);
+        }
+    }
+}
